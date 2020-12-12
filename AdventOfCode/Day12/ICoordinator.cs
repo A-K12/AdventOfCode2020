4ad2da817b4cb939a5d96d0a63efd079238a75e9
Alex <xalexseyx5@gmail.com>
2020-12-12 19:30:18 +0500

    Refactored Day 12

---
+++ AdventOfCode/Day12/ICoordinator.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2020.Day12
+{
+    public interface ICoordinator
+    {
+        (int x, int y) GetShipLocation();
+
+        void ProcessCommand(string command);
+    }
+}
--- AdventOfCode/Day12/Solution.cs
+++ AdventOfCode/Day12/Solution.cs
@@ -3,130 +3,141 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using AdventOfCode2020.Day12;
 
 namespace AdventOfCode2020.Day12
 {
-    class Solution:ITask
+    public class Solution:ITask
     {
         public void ExecuteTask()
         {
-            int[][] directions = new int[4][]
-            {
-                //север юг запад восток
-                new int[]{1,0,0},
-                new int[]{0,-1},
-                new int[]{-1,0},
-                new int[]{0, 1},
-            };
-            int dx = 10;
-            int dy = 1;
+            
             const string path = @".\Day12\data.txt";
             string[] lines = File.ReadAllLines(path);
-            int x = 0;
-            int y = 0;
-            int direction = 0;
-            for (int i = 0; i < lines.Length; i++)
+            
+
+            SimpleCoordinator coordinator1 = new SimpleCoordinator();
+            AdvancedCoordinator coordinator2 = new AdvancedCoordinator(10,1);
+
+            foreach (string line in lines)
             {
-                char command = lines[i][0];
-                int length = int.Parse(lines[i].Substring(1));
-                switch (command)
-                {
-                    case 'N':
-                        dy += length;
-                        break;
-                    case 'S':
-                        dy -= length;
-                        break;
-                    case 'E':
-                        dx += length;
-                        break;
-                    case 'W':
-                        dx -= length;
-                        break;
-                    case 'R':
-                        double rad1 = length * Math.PI / 180.0;
-                        double dxt1 = dx * Math.Cos(rad1) + dy * Math.Sin(rad1);
-                        double dyt1 = -dx * Math.Sin(rad1) + dy * Math.Cos(rad1);
-
-                        dx = (int)Math.Round(dxt1);
-                        dy = (int)Math.Round(dyt1);
-
-                        break;
-                    case 'L':
-                    
-                        double rad = length * Math.PI / 180.0;
-                        double dxt = dx * Math.Cos(rad) - dy * Math.Sin(rad);
-                        double dyt = dx * Math.Sin(rad) + dy * Math.Cos(rad);
-
-                        dx = (int)Math.Round(dxt);
-                        dy = (int)Math.Round(dyt);
-
-                        break;
-                    case 'F':
-                        x += dx * length;
-                        y += dy * length;
-                        break;
-                }
+                coordinator1.ProcessCommand(line);
+                coordinator2.ProcessCommand(line);
             }
 
-            Console.Out.WriteLine("x = {0}", Math.Abs(x)+Math.Abs(y));
+            (int x, int y) answer1 = coordinator1.GetShipLocation();
+            (int x, int y) answer2 = coordinator2.GetShipLocation();
+            Console.Out.WriteLine("Answer 1 = {0}", Math.Abs(answer1.x) + Math.Abs(answer1.y));
+            Console.Out.WriteLine("Answer 2 = {0}", Math.Abs(answer2.x)+Math.Abs(answer2.y));
         }
 
-        public void FirstTask()
-        {
-            int[][] directions = new int[4][]
-            {
-                new int[]{1,0},
-                new int[]{0,-1},
-                new int[]{-1,0},
-                new int[]{0, 1},
-            };
-            const string path = @".\Day12\data.txt";
-            string[] lines = File.ReadAllLines(path);
-            int x = 0;
-            int y = 0;
-            int direction = 0;
-            for (int i = 0; i < lines.Length; i++)
-            {
-                char command = lines[i][0];
-                int length = int.Parse(lines[i].Substring(1));
-                switch (command)
-                {
-                    case 'N':
-                        y += length;
-                        break;
-                    case 'S':
-                        y -= length;
-                        break;
-                    case 'E':
-                        x += length;
-                        break;
-                    case 'W':
-                        x -= length;
-                        break;
-                    case 'R':
-                        int count = length / 90;
-                        for (int j = 0; j < count; j++)
-                        {
-                            direction = direction == 3 ? 0 : direction + 1;
-                        }
-                        break;
-                    case 'L':
-                        int numLeft = length / 90;
-                        for (int j = 0; j < numLeft; j++)
-                        {
-                            direction = direction == 0 ? 3 : direction - 1;
-                        }
-                        break;
-                    case 'F':
-                        x += directions[direction][0] * length;
-                        y += directions[direction][1] * length;
-                        break;
-                }
-            }
+    }
 
-            Console.Out.WriteLine("x = {0}", Math.Abs(x) + Math.Abs(y));
+    public class AdvancedCoordinator : ICoordinator
+    {
+        public AdvancedCoordinator(int x, int y)
+        {
+            waypoint.dx = x;
+            waypoint.dy = y;
+        }
+
+        private (int dx, int dy) waypoint;
+        private (int x, int y) ShipLocation;
+
+        public (int x, int y) GetShipLocation() => ShipLocation;
+
+        public void ProcessCommand(string command)
+        {
+            int length = int.Parse(command.Substring(1));
+            char commandType = command[0];
+            switch (commandType)
+            {
+                case 'N':
+                    waypoint.dy += length;
+                    break;
+                case 'S':
+                    waypoint.dy -= length;
+                    break;
+                case 'E':
+                    waypoint.dx += length;
+                    break;
+                case 'W':
+                    waypoint.dx -= length;
+                    break;
+                case 'R':
+                    RotateWaypoint(length);
+                    break;
+                case 'L':
+                    RotateWaypoint(-length);
+                    break;
+                case 'F':
+                    ShipLocation.x += waypoint.dx * length;
+                    ShipLocation.y += waypoint.dy * length;
+                    break;
+            }
+        }
+
+        private void RotateWaypoint(double angle)
+        {
+            double rad1 = angle * Math.PI / 180.0;
+            double dxt1 = waypoint.dx * Math.Cos(rad1) + waypoint.dy * Math.Sin(rad1);
+            double dyt1 = -waypoint.dx * Math.Sin(rad1) + waypoint.dy * Math.Cos(rad1);
+
+            waypoint.dx = (int)Math.Round(dxt1);
+            waypoint.dy = (int)Math.Round(dyt1);
+        }
+    }
+
+
+
+    public class SimpleCoordinator : ICoordinator
+    {
+        public SimpleCoordinator()
+        {
+        }
+
+        public (int x, int y) GetShipLocation()
+        {
+            int x = handlers['N'] - handlers['S'];
+            int y = handlers['E'] - handlers['W'];
+
+            return (x, y);
+        }
+
+        private char[] directions = new char[4]
+        {
+            'E','S','W','N'
+        };
+
+        private readonly Dictionary<char,int> handlers = new Dictionary<char, int>()
+        {
+            {'N',  0},
+            {'S',  0},
+            {'E',  0},
+            {'W',  0},
+            {'R',  0},
+            {'L',  0},
+        };
+
+
+        public void ProcessCommand(string command)
+        {
+            int length = int.Parse(command.Substring(1));
+            char commandType = command[0];
+            if (commandType != 'F')
+            {
+                handlers[commandType] += length;
+            }
+            else
+            {
+                int route = (handlers['R']- handlers['L'])%360;
+                route /= 90;
+                route = route  == 4 ? 0 : route;
+                route = route  < 0 ? 3 + route : route;
+                handlers[directions[route]] += length;
+            }
         }
     }
 }
