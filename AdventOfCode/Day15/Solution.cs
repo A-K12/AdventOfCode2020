7a64b5bc4f52a202ab1fa197092fd86eac9261f2
Alex <xalexseyx5@gmail.com>
2020-12-15 21:16:19 +0500

    Day15

---
+++ AdventOfCode/Day15/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day15
+{
+    public class Solution:ITask
+    {
+        public void ExecuteTask()
+        {
+            const string path = @".\Day15\data.txt";
+            int[] numbers = File.ReadAllText(path).Split(',').Select(int.Parse).ToArray();
+                
+
+            Console.Out.WriteLine("Answer 1 = {0}", FindNumberInPosition(numbers, 2020));
+            Console.Out.WriteLine("Answer 1 = {0}", FindNumberInPosition(numbers, 30000000));
+        }
+
+
+        public int FindNumberInPosition(int[] startNumbers, int position)
+        {
+            Dictionary<int, int> map = startNumbers
+                .Select((key, value) => (key, value))
+                .ToDictionary(arg => arg.key, arg => arg.value);
+            int lastNum = 0;
+            for (int i = map.Count; i < position - 1; i++)
+            {
+                int newNum = map.ContainsKey(lastNum) ? i - map[lastNum] : 0;
+                map[lastNum] = i;
+                lastNum = newNum;
+            }
+
+            return lastNum;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,10 +9,7 @@ namespace AdventOfCode2020
     {
         private static void Main(string[] args)
         {
-            //nums[0] | ((long)1 << bitIndex) : nums[0] & ~((long)1 << bitIndex);
-
-            // var test = (char?)tst.GetValue(-1);
-            var solution = new Day14.Solution();
+            var solution = new Day15.Solution();
             solution.ExecuteTask();
         }
     }
